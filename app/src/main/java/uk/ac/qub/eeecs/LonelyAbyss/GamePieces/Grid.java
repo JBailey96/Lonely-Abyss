8f47e114fa20a720857e80a18c1023d04187f290
JBailey96 <jrbailey207@gmail.com>
2016-11-21 19:59:38 +0000

    Grid squares now change their bitmap when touched + Scaling issue with viewports sorted

--- app/src/main/java/uk/ac/qub/eeecs/LonelyAbyss/GamePieces/Grid.java
+++ app/src/main/java/uk/ac/qub/eeecs/LonelyAbyss/GamePieces/Grid.java
@@ -1,9 +1,12 @@
 package uk.ac.qub.eeecs.LonelyAbyss.GamePieces;
 
 import android.graphics.Bitmap;
+import android.graphics.Rect;
 
+import uk.ac.qub.eeecs.gage.util.Vector2;
 import uk.ac.qub.eeecs.gage.world.GameObject;
 import uk.ac.qub.eeecs.gage.world.GameScreen;
+
 
 /**
  * Created by James on 19/11/2016.
@@ -11,12 +14,28 @@ import uk.ac.qub.eeecs.gage.world.GameScreen;
 
 public class Grid extends GameObject {
     private boolean hidden; //true if the grid has not been selected, false if the grid has
+    private Bitmap realBitmap; //the actual bitmap displayed after the grid has been touched
+    private Rect posRect;
 
-    public Grid(float x, float y, float width, float height, Bitmap bitmap, GameScreen gameScreen, boolean hidden) {
+    public Grid(float x, float y, float width, float height, Bitmap bitmap, GameScreen gameScreen, boolean hidden, Bitmap realBitmap) {
         super(x, y, width, height, bitmap, gameScreen);
         this.hidden = hidden;
+        this.realBitmap = realBitmap;
     }
 
+    public boolean getHidden() {
+        return this.hidden;
+    }
+
+    public void setHidden(boolean b) {
+        this.hidden = b;
+    }
+
+    //called when the grid has been touched and is hidden
+    public void reveal() {
+        setHidden(false);
+        this.mBitmap = realBitmap;
+    }
 
 
 
--- app/src/main/java/uk/ac/qub/eeecs/LonelyAbyss/LevelCreator/GridLevel.java
+++ app/src/main/java/uk/ac/qub/eeecs/LonelyAbyss/LevelCreator/GridLevel.java
@@ -2,6 +2,8 @@ package uk.ac.qub.eeecs.LonelyAbyss.LevelCreator;
 
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 
 import java.util.List;
 
@@ -20,6 +22,7 @@ import uk.ac.qub.eeecs.gage.world.ScreenViewport;
  */
 
 public class GridLevel extends GameScreen {
+    //basic set up
     protected ScreenViewport mScreenViewport;
     protected LayerViewport mLayerViewport;
     protected Input mInput;
@@ -28,7 +31,7 @@ public class GridLevel extends GameScreen {
 
     //types of grid squares
     public enum GridType {
-        START, END, EMPTY, UNIMON, BATTLE, LOCATION, MOVEMENT, TRADE;
+        START, END, EMPTY, UNIMON, BATTLE, LOCATION, MOVEMENT, TRADE, HIDDEN
     }
 
     //number of grid squares to be generated
@@ -42,58 +45,61 @@ public class GridLevel extends GameScreen {
     private final int gridRows = 4;
 
     //gridArray to hold all the grid squares
-    private Grid[][] gridArray = new Grid[4][4];
+    private Grid[][] gridArray;
 
     public GridLevel(Game game) {
         super("GridLevel", game);
-        mLayerViewport = new LayerViewport(500, 500, game.getScreenWidth()/2, game.getScreenHeight()/2);
+        mLayerViewport = new LayerViewport(game.getScreenWidth()/2, game.getScreenHeight()/2, game.getScreenWidth()/2, game.getScreenHeight()/2);
         mScreenViewport = new ScreenViewport(0, 0, game.getScreenWidth(), game.getScreenHeight());
-        loadGridBitmaps();
-        generateGrids();
+
+        gridArray = new Grid[gridRows][gridColumns];
+        loadGridBitmaps(); //load grid tile bitmaps into asset manager
+        generateGrids(); //generate the grid tiles in preparation of being displayed
     }
 
     @Override
     public void update(ElapsedTime elapsedTime) {
         mInput = mGame.getInput();
         touchEvents = mInput.getTouchEvents();
+        touchGrid(touchEvents); //checks if the user has touched a grid.
     }
 
     @Override
     public void draw(ElapsedTime elapsedTime, IGraphics2D graphics2D) {
-        graphics2D.clear(Color.WHITE);
+        graphics2D.clear(Color.BLACK);
         graphics2D.clipRect(mScreenViewport.toRect());
 
-        for (int i = 0; i < gridColumns; i++) {
-            for (int j = 0; j < gridRows; j++) {
+        //draw grid bitmap tiles
+        for (int i = 0; i < gridRows; i++) {
+            for (int j = 0; j < gridColumns; j++) {
                 gridArray[i][j].draw(elapsedTime, graphics2D, mLayerViewport, mScreenViewport);
             }
         }
-        //Grid end = new Grid(50, 50, 100, 100, selectBitmap(GridType.END), this, true);
-
-
-        //drawing background?
-       // graphics2d.drawBitmap(background, null,
-         //       DwarfHomelandImg, null);
-
-
-
     }
 
+    //generate a number of grids
     public void generateGrids() {
         float gridWidth = mLayerViewport.getWidth()/gridColumns;
         float gridHeight = mLayerViewport.getHeight()/gridRows;
 
-        float b = commonDivisor(mLayerViewport.getWidth(), mLayerViewport.getHeight());
-        float x = mLayerViewport.getLeft();
-        float y = mLayerViewport.getTop();
+        //float squareRectRegionWidth = gridWidth*0.95f;
+        float squareRectRegionHeight = gridHeight*0.95f;
 
-        for (int i = 0; i < gridColumns; i++) {
-            for (int j = 0; j < gridRows; j++) {
-                gridArray[i][j]  = new Grid(x, y, gridWidth, gridHeight, selectBitmap(GridType.START), this, true);
+        //the bitmap grid image that is revealed when the user touches a grid
+        Bitmap hiddenBitmap = getGame().getAssetManager().getBitmap("HIDDEN");
+
+        //initial position of the first grid tile
+        float x = mLayerViewport.getLeft() + gridWidth/2;
+        float y = mLayerViewport.getTop() - gridHeight/2;
+
+        //fill the 2d array grid objects
+        for (int i = 0; i < gridRows; i++) {
+            for (int j = 0; j < gridColumns; j++) {
+                gridArray[i][j]  = new Grid(x, y, squareRectRegionHeight, squareRectRegionHeight, hiddenBitmap, this, true, selectBitmap(GridType.START)); //generate a new grid object
                 x = x + gridWidth;
             }
-            x = mLayerViewport.getLeft();
-            y = y-gridHeight;
+            x = mLayerViewport.getLeft() + gridWidth/2;
+            y = y - gridHeight;
         }
     }
 
@@ -129,10 +135,15 @@ public class GridLevel extends GameScreen {
                 bitmap = "TRADE";
                 break;
             }
+            case HIDDEN: {
+                bitmap = "HIDDEN";
+                break;
+            }
         }
         return getGame().getAssetManager().getBitmap(bitmap);
     }
 
+    //load the grid square bitmaps into the asset manager to use to create the grid squares.
     public void loadGridBitmaps() {
         getGame().getAssetManager().loadAndAddBitmap("START", "img/GridSquares/Start.png");
         getGame().getAssetManager().loadAndAddBitmap("END", "img/GridSquares/End.png");
@@ -142,10 +153,25 @@ public class GridLevel extends GameScreen {
         getGame().getAssetManager().loadAndAddBitmap("MOVEMENT", "img/GridSquares/Movement.png");
         getGame().getAssetManager().loadAndAddBitmap("TRADE", "img/GridSquares/Trade.png");
         getGame().getAssetManager().loadAndAddBitmap("UNIMON", "img/GridSquares/Unimon.png");
+        getGame().getAssetManager().loadAndAddBitmap("HIDDEN", "img/GridSquares/Hidden.png");
     }
 
-    public float commonDivisor(float x, float y) {
-        if (y==0) return x;
-        return commonDivisor(y,x%y);
+    //handling a grid being touched
+    public void touchGrid(List<TouchEvent> touchEvents) {
+        int numTouchEvent = touchEvents.size();
+
+        for  (TouchEvent t: touchEvents) {
+            if (t.type == TouchEvent.TOUCH_UP) {
+                for (int i = 0; i < gridRows; i++) {
+                   for (int j = 0; j < gridColumns; j++) {
+                       if ((gridArray[i][j].getHidden()) && (gridArray[i][j].getBound().contains((int) t.x, (int) mLayerViewport.getTop()-t.y))) { //checks whether a grid is hidden and the user is touching a grid
+                           gridArray[i][j].reveal();
+                       }
+                   }
+                }
+            }
+        }
+
     }
+
 }
